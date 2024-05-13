86c70e7565f4a18f947790ab5d2815efa2105712
Águeda Muela <agmuelamar@gmail.com>
2024-05-13 12:54:10 +0200

    food planning list

--- src/components/FoodPlanningCard.jsx
+++ src/components/FoodPlanningCard.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
-function FoodPlanningCard() {
+function FoodPlanningCard(props) {
+  // destructuring
+  const { title, description, image } = props.eachFoodPlanning;
+
   return (
-    <div>FoodPlanningCard</div>
-  )
+    // <div style={containerCardStyles}>
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src={image} />
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Text>{description}</Card.Text>
+          <Button variant="primary">Details</Button>
+        </Card.Body>
+      </Card>
+  );
 }
 
-export default FoodPlanningCard
+export default FoodPlanningCard;
